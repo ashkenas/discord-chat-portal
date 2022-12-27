02fc3a4b854ba71e305d1fb95a24fe447d411f34
Jacob Ashkenas <jacobashkenas@gmail.com>
2022-12-27 01:46:09 -0500

    Posting to webhook

--- src/main/java/com/jacobashkenas/dcp/mixins/ChatMixin.java
+++ src/main/java/com/jacobashkenas/dcp/mixins/ChatMixin.java
@@ -1,6 +1,6 @@
 package com.jacobashkenas.dcp.mixins;
 
-import com.mojang.logging.LogUtils;
+import com.jacobashkenas.dcp.DiscordChatPortal;
 import net.minecraft.network.message.MessageType;
 import net.minecraft.network.message.SignedMessage;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
@@ -9,10 +9,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
+@SuppressWarnings({"unused"})
 @Mixin(ServerPlayNetworkHandler.class)
 public class ChatMixin {
     @Inject(method = "sendChatMessage", at = @At("RETURN"))
     private void sendChatMessage(SignedMessage message, MessageType.Parameters params, CallbackInfo ci) {
-        LogUtils.getLogger().info("Got Message: " + message.getContent().getString());
+        if (params.targetName() != null) return;
+
+        DiscordChatPortal.INSTANCE.postMessage(
+                message.getContent().getString(),
+                params.name().getString(),
+                message.getSender().toString()
+        );
     }
 }
--- src/main/kotlin/com/jacobashkenas/dcp/DiscordChatPortal.kt
+++ src/main/kotlin/com/jacobashkenas/dcp/DiscordChatPortal.kt
@@ -1,9 +1,32 @@
 package com.jacobashkenas.dcp
+import com.google.gson.Gson
 import com.mojang.logging.LogUtils
 import net.fabricmc.api.ModInitializer
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse.BodyHandlers
 
+@Suppress("unused")
 object DiscordChatPortal: ModInitializer {
     private const val MOD_ID = "dcp"
+    private const val URL = "webhook URL"
+
+    fun postMessage(message: String, username: String, uuid: String) {
+        val request = HttpRequest.newBuilder(URI(URL))
+            .headers("Content-Type", "application/json")
+            .POST(HttpRequest.BodyPublishers.ofString(Gson().toJson(object {
+                val content = message
+                val username = username
+                val avatar_url = "https://mc-heads.net/head/$uuid/left"
+                val allowed_mentions = object {
+                    val parse = IntArray(0)
+                }
+            })))
+            .build()
+
+        HttpClient.newBuilder().build().sendAsync(request, BodyHandlers.discarding())
+    }
 
     override fun onInitialize() {
         LogUtils.getLogger().info("Initialized.")
